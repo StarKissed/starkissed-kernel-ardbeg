df18c126d3fcdc6bbcbe81fb4282ad5b1d1a2483
TwistedUmbrella <twistedumbrella@gmail.com>
2014-11-23 00:20:50 -0500

    build: adjustments for tegra dtb implementation

--- buildimg/img.sh
+++ buildimg/img.sh
@@ -9,5 +9,6 @@ BUILDSTRUCT=darwin
 fi
 
 chmod a+r zImage
+chmod a+r tegra124-ardbeg.dtb
 $BUILDSTRUCT/./mkbootfs boot.img-ramdisk | gzip > newramdisk.cpio.gz
-$BUILDSTRUCT/./mkbootimg --cmdline 'no_console_suspend=1' --kernel zImage --ramdisk newramdisk.cpio.gz -o boot.img --base 0x10000000
+$BUILDSTRUCT/./mkbootimg --cmdline 'no_console_suspend=1 console=null' --kernel zImage --ramdisk newramdisk.cpio.gz -o boot.img --base 0x10000000 --pagesize 2048 --ramdisk_offset 0x11000000 --tags_offset 0x10f00000 --dt tegra124-ardbeg.dtb
