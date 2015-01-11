6018fb6873598103e272da872b8eb83d214bb919
TwistedUmbrella <twistedumbrella@gmail.com>
2015-01-11 17:37:33 -0500

    cpu_freq: apply cap changes to all speedo values

--- arch/arm/mach-tegra/tegra13_speedo.c
+++ arch/arm/mach-tegra/tegra13_speedo.c
@@ -139,11 +139,7 @@ void tegra_init_speedo_data(void)
 		return;
 	}
 
-#ifdef CONFIG_TEGRA_CPU_CAP_OVERCLOCK
-    cpu_speedo_0_value = 2500;
-#else
 	cpu_speedo_0_value = tegra_fuse_readl(FUSE_CPU_SPEEDO_0);
-#endif
 	cpu_speedo_1_value = tegra_fuse_readl(FUSE_CPU_SPEEDO_1);
 
 	/* GPU Speedo is stored in CPU_SPEEDO_2 */
@@ -157,7 +153,11 @@ void tegra_init_speedo_data(void)
 	soc_iddq_value = tegra_fuse_readl(FUSE_SOC_IDDQ);
 	gpu_iddq_value = tegra_fuse_readl(FUSE_GPU_IDDQ);
 
+#ifdef CONFIG_TEGRA_CPU_CAP_OVERCLOCK
+    cpu_speedo_value = 2500;
+#else
 	cpu_speedo_value = cpu_speedo_0_value;
+#endif
 
 	if (cpu_speedo_value == 0) {
 		cpu_speedo_value = 2100;
